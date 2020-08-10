995ea27f9094066901d1e815592a7b3e813cc610
syedbilal28 <syed.bilal.sba@gmail.com>
2020-08-11 01:04:02 +0500

    "Login Signup and Logout"

--- pizza/order/models.py
+++ pizza/order/models.py
@@ -1,20 +1,33 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
 class Pizza(models.Model):
     Flavor = models.CharField(max_length = 20)
     size = models.CharField(max_length=10)
     toppings= models.IntegerField()
+    quantity=models.IntegerField()
     def __str__(self):
         return " Flavor: {}, Size: {}".format(self.Flavor,self.size)
 
 
 class Customer(models.Model):
-    name = models.CharField(max_length=30)
-    contact = models.CharField(max_length=11)
+
+    user = models.OneToOneField(User,on_delete=models.CASCADE)
+    contact = models.CharField(max_length=11,unique=True)
+    def __str__(self):
+        return self.name
+
+
 
 class Order(models.Model):
     order_no = models.CharField(max_length=4)
     date_time = models.DateTimeField()
     customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
     pizza = models.ForeignKey(Pizza,on_delete=models.CASCADE)
+    def __str__(self):
+        return "{},{}, {}".format(self.order_no,self.pizza,self.customer)
+class OrderQueue(models.Model):
+    id = models.OneToOneField(Order, on_delete=models.CASCADE,primary_key=True,unique = True)
+
+    def __str__(self):
+        return "{}".format(str(self.id))
--- pizza/order/views.py
+++ pizza/order/views.py
@@ -1,5 +1,38 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Customer
+from django.contrib.auth import login,authenticate,logout
 
+from django.contrib.auth.models import User
 # Create your views here.
-def index(request):
-    return ("Hello World")
+def Menu(request):
+    return render(request,"menu.html")
+def Signup_page(request):
+    return render(request,"signup.html")
+def Login_page(request):
+    return render(request,'login.html')
+def SignUp(request):
+    username = request.POST.get("customer_name",False)
+    password = request.POST.get("password",False)
+    number= request.POST.get("contact_number",False)
+    user= User(name=username,password=password)
+    customer= Customer(user=user,contact=number)
+    user.save()
+    customer.save()
+    login(request,customer)
+    return render(request,"login.html")
+def Login(request):
+    username= request.POST.get("customer_name",False)
+    password = request.POST.get("password",False)
+    print(password)
+    user = authenticate(username=username,password=password)
+    if user is True:
+        login(request,user)
+        return render(request,"menu.html")
+    else:
+        return render(request,"error.html")
+def Logout(request):
+    logout(request)
+    return render(request,"menu.html")
+# def customer_order(request):
+#     orders = Order.objects.all(name= )
