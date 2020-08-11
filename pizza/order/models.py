e8b0eaeda97e7ba5a87f2806cbea4d4328b365c6
syedbilal28 <syed.bilal.sba@gmail.com>
2020-08-11 21:15:55 +0500

    "Model Improvement for Pizza orders"

--- pizza/order/models.py
+++ pizza/order/models.py
@@ -3,27 +3,34 @@ from django.contrib.auth.models import User
 # Create your models here.
 class Pizza(models.Model):
     Flavor = models.CharField(max_length = 20)
-    size = models.CharField(max_length=10)
-    toppings= models.IntegerField()
-    quantity=models.IntegerField()
+    # size = models.CharField(max_length=10)
+    # toppings= models.IntegerField()
+    # quantity=models.IntegerField()
     def __str__(self):
-        return " Flavor: {}, Size: {}".format(self.Flavor,self.size)
-
+        return " Flavor: {}".format(self.Flavor)
+class Toppings(models.Model):
+    topping= models.CharField(max_length=256)
+    def __str__(self):
+        return self.topping
 
 class Customer(models.Model):
     user = models.OneToOneField(User,on_delete=models.CASCADE)
     contact = models.CharField(max_length=11,unique=True)
 
-
+class Item(models.Model):
+    pizza= models.ForeignKey(Pizza,on_delete=models.CASCADE)
+    size= models.CharField(max_length=256,choices=[('Large','Large'),('Medium','Medium'),('Small','Small')])
+    quantity= models.IntegerField()
+    topping= models.OneToOneField(Toppings,on_delete=models.CASCADE)
 
 
 class Order(models.Model):
-    order_no = models.CharField(max_length=4)
+
     date_time = models.DateTimeField()
     customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
-    pizza = models.ForeignKey(Pizza,on_delete=models.CASCADE)
+    Items = models.ForeignKey(Item,on_delete=models.CASCADE,default=False)
     def __str__(self):
-        return "{},{}, {}".format(self.order_no,self.pizza,self.customer)
+        return "{},{}".format(self.pk,self.customer)
 class OrderQueue(models.Model):
     id = models.OneToOneField(Order, on_delete=models.CASCADE,primary_key=True,unique = True)
 
--- pizza/order/tests.py
+++ pizza/order/tests.py
@@ -1,14 +1,14 @@
 from django.test import TestCase,Client
-from .models import Customer
+from .models import Customer,Pizza,Item,Order,Toppings
 from django.contrib.auth.models import User
 from django.contrib.auth import login,authenticate
-
+import datetime
 # Create your tests here.
 class OrderTest(TestCase):
     def setUp(self):
         # Creating user and Customer
         self.user= User.objects.create_user(username="be red",password="123456")
-        customer= Customer.objects.create(user=self.user,contact="11111")
+        self.customer= Customer.objects.create(user=self.user,contact="11111")
     def test1(self):
 
         user= authenticate(username="be red",password="123456")
@@ -18,6 +18,14 @@ class OrderTest(TestCase):
         # login(request,user=user)
     def test2(self):
         customers= Customer.objects.all()
+    def test3(self):
+        Flavor = "Sicilan Cheese"
+        Flavor = Pizza.objects.create(Flavor=Flavor)
+        t1= Toppings.objects.create(topping="Jalapeno")
+        I1= Item.objects.create(pizza=Flavor,size="Large",quantity=1,topping=t1)
+        O1= Order.objects.create(customer=self.customer,date_time=datetime.datetime.now(),Items=I1)
+        self.assertEqual(O1.pk,1)
+
 
 
 
